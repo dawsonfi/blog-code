07c7ba77d9c1834429c2a27ebb69250b310cf6dc
Dawson Israel <dawfre@amazon.com>
2022-03-09 15:18:02 -0300

    Fix lambda deploy commands

--- 01-lambda/rust-lambda/src/main.rs
+++ 01-lambda/rust-lambda/src/main.rs
@@ -11,5 +11,5 @@ async fn main() -> Result<(), Error> {
 async fn func(event: Value, _: Context) -> Result<Value, Error> {
     let first_name = event["firstName"].as_str().unwrap_or("world");
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+    Ok(json!({ "message": format!("Hi, {}!", first_name) }))
 }
